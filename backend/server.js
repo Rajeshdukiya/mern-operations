731abcd7e6aa53944b26dc2cb9c3e9c1436b2876
Rajesh Dukiya <112774887+Rajeshdukiya@users.noreply.github.com>
2024-08-19 23:10:20 +0800

    middleware-create

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,13 @@
 const express = require('express');
+const { errorHandler } = require('./middleware/errorMiddleware');
 const dotenv = require('dotenv').config();
 const port = process.env.PORT || 5000
 const app = express();
 
-// app.get('/api/goals', (req,res)=>{
-//     res.send('Get goals')
-// })
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
 
-// app.get('/api/goals', (req, res)=> {
-//     res.status(200).json({massage: "get goals"})
-// })
 app.use('/api/goals', require('./routes/goalRoutes'))
-
-
+app.use(errorHandler)
 
 app.listen(port, () => console.log(`server started on port gitignore ${port}`))
