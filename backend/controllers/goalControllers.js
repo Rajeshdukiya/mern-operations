b959bed4136d117d2f41cf7cdcb6247d4ae55d43
Rajesh Dukiya <112774887+Rajeshdukiya@users.noreply.github.com>
2024-08-19 23:16:52 +0800

    express-async-handler

--- backend/controllers/goalControllers.js
+++ backend/controllers/goalControllers.js
@@ -1,35 +1,37 @@
+const asyncHandler = require('express-async-handler')
+
 // @desc Get goals
 // @route GET /api/goals
 // @acces private
-const getGoals = (req, res) =>{
+const getGoals = asyncHandler( async (req, res) =>{
     res.status(200).json({massage: "get goals"})
-}
+})
 
 // @desc Set goals
 // @route POST /api/goals
 // @acces private
-const setGoals = (req, res) =>{
+const setGoals = asyncHandler( async (req, res) =>{
     if (!req.body.text) {
         res.status(400)
         throw new Error('Please add a text field')
     }
 
     res.status(200).json({massage: "set goal"})
-}
+})
 
 // @desc Update goals
 // @route PUT /api/goals/:id
 // @acces private
-const updateGoals = (req, res) =>{
+const updateGoals = asyncHandler( async (req, res) =>{
     res.status(200).json({massage: `Update goals ${req.params.id}`})
-}
+})
 
 // @desc Delete goals
 // @route DELETE /api/goals/:id
 // @acces private
-const deleteGoals = (req, res) =>{
+const deleteGoals = asyncHandler( async (req, res) =>{
     res.status(200).json({massage: `Delete goals ${req.params.id}`})
-}
+})
 
 module.exports = {
     getGoals,
