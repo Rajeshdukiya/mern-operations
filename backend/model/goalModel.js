c39383d0281780c1bac29c2b3856c45820e00ecc
Rajesh Dukiya <112774887+Rajeshdukiya@users.noreply.github.com>
2024-08-20 10:09:21 +0800

    add-user-module

--- backend/model/goalModel.js
+++ backend/model/goalModel.js
@@ -1,9 +1,16 @@
 const { text } = require('body-parser')
 const { timeStamp } = require('console')
 const mongoose = require('mongoose')
+const { type } = require('os')
 
 const goalSchema = mongoose.Schema(
-    {
+    {   
+        user:{
+            type: mongoose.Schema.typeObjectId,
+            required: true,
+            ref: 'User'
+        },
+
         text: {
         type: String,
         required: [true, 'Please add a text value'],
