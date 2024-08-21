573910f2dc7ba7906ff95e56d420f481feccd2d9
Rajesh Dukiya <112774887+Rajeshdukiya@users.noreply.github.com>
2024-08-21 16:42:22 +0800

    authenticate-user

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -42,7 +42,7 @@ const registerUser = asyncHandler(async (req, res) =>{
         })
     } else {
         res.status(400)
-        throw new Error('INvalid user data')
+        throw new Error('Invalid user data')
     }
    
 })
@@ -51,7 +51,21 @@ const registerUser = asyncHandler(async (req, res) =>{
 // @route POST/api/users/login
 // @access public
 const loginUser = asyncHandler(async (req,res) =>{
-    res.json({ message: 'Login User'})
+    const { email, password } = req.body
+
+    // Check for user email
+    const user = await User.findOne({email})
+
+    if(user && (await bcrypt.compare(password, user.password))) {
+        res.json({
+            _id: user.id,
+            name: user.name,
+            email: user.email,
+        })
+    } else {
+        res.status(400)
+        throw new Error('Invalid credentials')
+    }
 })
 
 // @desec Get user data
