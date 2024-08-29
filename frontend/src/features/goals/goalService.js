d4133f9adebc66aa98970cd3cd690279bd22ed84
Rajesh Dukiya <112774887+Rajeshdukiya@users.noreply.github.com>
2024-08-29 14:15:43 +0530

    creategoal-async

--- frontend/src/features/goals/goalService.js
+++ frontend/src/features/goals/goalService.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import axios from 'axios'
 
-const goalService = () => {
-  return (
-    <div>goalService</div>
-  )
+const API_URL = '/api/goals'
+
+// Create new goal
+const createGoal = async (goalData, token) => {
+  const config = {
+    headers: {
+      Authorization: `Bearer ${token}`
+    }
+  }
+
+  const response = await axios.post(API_URL, goalData, config)
+
+  return response.data
+}
+
+const goalService = {
+  createGoal
 }
 
 export default goalService
--- frontend/src/features/goals/goalSlice.js
+++ frontend/src/features/goals/goalSlice.js
@@ -1,6 +1,5 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 
-
 const initialState = {
     goals: [],
     isError: false,
@@ -8,6 +7,23 @@ const initialState = {
     isLoading: false,
     message: ''
 }
+
+// Create new goal
+export const createGoal = createAsyncThunk('goals/create', async (goalData, thunkAPI) => {
+    try {
+      const token = thunkAPI.getState().auth.user.token
+      return await goalService.createGoal(goalData,token)
+    } catch (error) {
+        const message =
+          (error.response &&
+            error.response.data &&
+            error.response.data.message) ||
+          error.message ||
+          error.toString();
+        return thunkAPI.rejectWithValue(message);
+    }
+    )
+})
 
 export  const  goalSlice = createSlice({
     name: 'goal',
